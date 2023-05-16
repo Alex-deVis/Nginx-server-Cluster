fea67ebded9573d834348c0ce3dacf005e86ae44
Alex Apostolescu <alexx.apostolescu@gmail.com>
2023-05-16 16:29:52 +0000

    Add script to run all

---
+++ one-to-rule-them-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCGC_HOME=$(dirname $(realpath "$0"))
+NGINX_PATH="$SCGC_HOME/nginx"
+PROMEXPORTER_PATH="$SCGC_HOME/promexporter"
+PROMETHEUS_PATH="$SCGC_HOME/prometheus"
+GRAFANA_PATH="$SCGC_HOME/grafana"
+
+
+if [[ "$1" == "clean" ]]; then
+	bash "$GRAFANA_PATH/destroy-grafana.sh"
+	bash "$PROMETHEUS_PATH/destroy-prometheus.sh"
+	bash "$PROMEXPORTER_PATH/destroy-promexporter.sh"
+	bash "$NGINX_PATH/destroy-nginx.sh"
+	bash "$SCGC_HOME/destroy-cluster.sh"
+else
+	bash "$SCGC_HOME/deploy-cluster.sh"
+	bash "$NGINX_PATH/deploy-nginx.sh"
+	bash "$PROMEXPORTER_PATH/deploy-promexporter.sh"
+	bash "$PROMETHEUS_PATH/deploy-prometheus.sh"
+	bash "$GRAFANA_PATH/deploy-grafana.sh"
+fi
+
